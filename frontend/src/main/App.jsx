3393045e132c88d30211752f9b6c4b36bcf112c2
DavidIsrael <davidisrael.rj@gmail.com>
2024-09-01 20:38:53 -0300

    Inserindo icone

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,14 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+
 import Footer from '../components/template/Footer'
 import Logo from '../components/template/Logo'
 import Main from '../components/template/Main'
 import Nav from '../components/template/Nav'
-import './App.css'
-import React from 'react'
 
 export default props =>
     <div className="app">
         <Logo/>
         <Nav/>
-        <Main/>
+        <Main icon="shopping-cart" title="Início"
+            subtitle="Cadastro de Produtos"/>
         <Footer/>
     </div>
