7bcf39a5fbf54237f56531968c2c7451fd3a062b
DavidIsrael <davidisrael.rj@gmail.com>
2024-09-02 08:41:03 -0300

    Menu

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -3,5 +3,15 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <nav className='menu'>
+            <a href="#/">
+                <i className='fa fa-home'></i> Início
+            </a>
+            <a href="#/produto">
+                <i className='fa fa-cart-plus'></i> Produto
+            </a>
+            <a href="#/setores">
+                <i className='fa fa-shopping-bag'></i> Setores
+            </a>
+        </nav>
     </aside>
